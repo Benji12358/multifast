0fd1d8713b84d9d1bbd9122a08e2f75b25521a4e
Benjamin.Arrondeau <benji.arrondeau@gmail.com>
2022-03-07 18:16:55 +0100

    reset Simmulation example

--- Simulation/Example/Input/Settings/discretization.d
+++
@@ -1,4 +0,0 @@
-1						schemes 0=O2, 1=EXPLICIT OPTIMIZED, 2=EXPLICIT OPTIMIZED WITH CPT AT BOUNDARIES, 3=COMPACT_SCHEMES
-3	0.001	          	time_schem dt
-0						Poisson solver (0: physical, 1: purely spectral)
-
