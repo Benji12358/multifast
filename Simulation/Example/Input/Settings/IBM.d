6f5bfa2a6c4facbe5f4663bd05dedbb4a7c4cc0d
Benjamin.Arrondeau <benji.arrondeau@gmail.com>
2022-04-05 09:50:08 +0200

    add few options for running simulations

--- Simulation/Example/Input/Settings/IBM.d
+++ Simulation/Example/Input/Settings/IBM.d
@@ -1,4 +1,5 @@
 1		IBM_activated: 0 (no) 1 (yes)
+400.0		ren_tau
 1		number_of_objects in the domain
 3		interpol_type (0: no interpol, 2: antisymmetric interpol)
 /fsnet/people/arrondea7b/project/21MULTIFAST/WORKSPACE/Codes/DNS/Simulations/IBM_DRP_2nd_refine/Input/OBJ_files/cube.obj
--- Simulation/Example/Input/Settings/computational_domain.d
+++ Simulation/Example/Input/Settings/computational_domain.d
@@ -1,4 +1,5 @@
 257 129 129		 n1  n2  n3 
 1.25   2.0  2.5      L3/pi 	L2 	L1/pi
 3.8		1	 stretch_Y
+0				scale L1 & L3 with Pi (0: No, 1:Yes)
 
