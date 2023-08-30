fb597ebbdac7ddf9a371156ead69abdcf6c9842d
Benjamin.Arrondeau <benji.arrondeau@gmail.com>
2023-08-30 12:36:39 +0200

    clean repo

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IT1=101000
-EVERY=1000
-IT2=200000
-
-for numero in `seq $IT1 $EVERY $IT2`
-    do rm ./field"$numero"/Elec* ./field"$numero"/void* ./field"$numero"/sca* ./field"$numero"/Sca*
-done
--- remake.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-make
-./job_laptop_singleproc.sh $1 $2 $3 $4
